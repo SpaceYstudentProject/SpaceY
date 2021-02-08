f01dbc8cd99167a372c9d414e82bdcce47cddd9e
Diman <vector_diman@mail.ru>
2021-02-08 17:50:57 +0300

    add: routing v3.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MissionComponent } from './mission/mission.component';
+import { NewsComponent } from './news/news.component';
+import { StarshipComponent } from './starship/starship.component';
 
 
+const routes: Routes = [
+  { path: 'mission', component: MissionComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'starship', component: StarshipComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
