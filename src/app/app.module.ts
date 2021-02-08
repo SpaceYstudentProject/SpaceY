4ae1b3464e7b9d3787f49c7dc0945136148b760c
Diman <vector_diman@mail.ru>
2021-02-08 17:20:24 +0300

    add: routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { MissionComponent } from './mission/mission.component';
+import { StarshipComponent } from './starship/starship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    MissionComponent,
+    StarshipComponent
   ],
   imports: [
     BrowserModule
